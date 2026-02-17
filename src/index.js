13f7300713e37d78ef4d95a936cad194b1fd634d
Denis <bezsaluy@gmail.com>
2026-02-17 14:32:04 +0200

    Finish

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
-'use strict'
+'use strict';
+
+/**
+ * 
+ * @param {number} num - целое неотрицательное число
+ * @returns Факториал числа (num)
+ */
+
+function factorial(num){
+
+    if(num < 0) return NaN;
+
+    if(num === 0 || num === 1) return 1;
+
+    return num * factorial(num - 1);
+
+}
+
+console.log(factorial(5));
+console.log(factorial(3));
+console.log(factorial(2));
+console.log(factorial(6));
+console.log(factorial(8));
